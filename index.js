dc1e7b38fdd14c6e57c57570f7a89f8b6b122190
Daryl Hepting <heptingd@acm.org>
2021-12-18 10:23:10 -0600

    cleaned up a few things

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import ItemApp from "./ItemApp";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<ItemApp />, rootElement);
-
